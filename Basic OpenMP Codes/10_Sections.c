1a65b05c9c1eebdc4548bd5ee67c6397a6feeae3
Yogesh <38850781+Yogesh17IITM@users.noreply.github.com>
2018-05-01 17:25:39 +0530

    Add files via upload

--- Basic OpenMP Codes/10_Sections.c
+++ Basic OpenMP Codes/10_Sections.c
@@ -1,4 +1,6 @@
 // This program illustrates how to split-up the work among team members (i.e., threads) using 'section'  directive
+// Please note that we do not have control on which thread to do a particular task (i.e.,) by default the thread will be selected arbitrarily
+// In the following example, 4 threads are specified, out of which 2 are shared.
 
 #include<stdio.h>
 #include<omp.h>
@@ -24,6 +26,6 @@ main()
           printf("Section 2 is handled by thread %d out of %d \n",tid,nth);
         }
     }
-  }  
-  printf("\n Please note that the order is NOT DETERMINISTIC \n");
+  }
+  printf("\n Please note that the order (of thread) is NOT DETERMINISTIC \n");
 }
